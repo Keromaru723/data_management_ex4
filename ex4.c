cb5f949e42aeb5cb5b54a122b41b54acc7fd4627
Keromaru723 <87127435+Keromaru723@users.noreply.github.com>
2021-07-08 21:39:40 +0900

    Greetings added

--- ex4.c
+++ ex4.c
@@ -4,6 +4,13 @@
 int main(){
   srand((unsigned int)time(NULL));
   int i,total,die[2];
+  char name[8];
+
+  printf("What your name\n");
+  scanf("%s",name);
+  printf("Hello, %s\n",name);
+
+  
   printf("Rolling the dice...\n");
   for(i=0;i<2;i++){
     die[i]= rand() % 6 + 1;
